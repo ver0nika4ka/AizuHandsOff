d6a8abf32f5dee66e81236455833e94a265b7c37
Veronica <veranika.aizu@gmail.com>
2019-06-28 11:47:30 +0900

    Added contact_info to Owner

--- AizuHandsOff/HandsOffApp/models.py
+++ AizuHandsOff/HandsOffApp/models.py
@@ -5,6 +5,7 @@ class Owner(models.Model):
 	email = models.EmailField()
 	# Hashed password 
 	password = models.CharField(max_length=128)
+	contact_info = models.CharField(max_length=256)
 
 	def __str__(self):
 		return self.name
@@ -20,7 +21,6 @@ class Item(models.Model):
 	category = models.ForeignKey(Category,on_delete=models.CASCADE)
 	name = models.CharField(max_length=64)
 	description = models.CharField(max_length=512)
-	contact_info = models.CharField(max_length=256)
 	available_date = models.DateField()
 	price = models.CharField(max_length=64)
 
